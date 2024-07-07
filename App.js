a4d36b30c700720ac9c6e8fb66e928cc58d3929c
rohitrajiit <rohitraj.iitm@gmail.com>
2024-07-07 16:26:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/App.js
+import React from 'react';
+import './App.css';
+import ChessGame from './components/Chessboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chess Game</h1>
+        <ChessGame />
+      </header>
+    </div>
+  );
+}
+
+export default App;
